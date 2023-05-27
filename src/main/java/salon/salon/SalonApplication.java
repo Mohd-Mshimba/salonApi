8e4cbc0a3542219e8c01c0d3e6cb358a5309f13f
Moh'd Mshimba Seif <mohd.seif@egaz.go.tz>
2023-05-27 20:52:36 +0300

    everything work fine

--- src/main/java/salon/salon/SalonApplication.java
+++ src/main/java/salon/salon/SalonApplication.java
@@ -1,9 +1,19 @@
 package salon.salon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Salon API",
+		description = "This is a Spring Boot application Api for a salon.",
+		version = "1.0",
+		contact = @Contact(name =":"+ " Moh'd Mshimba", email = "mohdmshimba95@gmail.com")
+))
 public class SalonApplication {
 
 	public static void main(String[] args) {
